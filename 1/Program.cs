ededed5c30f3e36c3ed4e69a4d07a005ba2c0665
gnom <154268818+andruha332@users.noreply.github.com>
2024-03-18 16:02:29 +0300

    version 0.1

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class student
+    {
+        public string name;
+        public int kurse;
+        public bool sex;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
