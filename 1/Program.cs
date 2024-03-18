18e9495dde16d2c844c5ef996ba6dc2001707a8e
gnom <154268818+andruha332@users.noreply.github.com>
2024-03-18 17:05:27 +0300

    version 0.4

--- 1/Program.cs
+++ 1/Program.cs
@@ -11,16 +11,18 @@ namespace _1
         public string name;
         public int kurse;
         public bool sex;
-
+        public student(string name, int kurse, bool sex)
+        {
+            this.name = name;
+            this.kurse = kurse;
+            this.sex = sex;
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            student student = new student();
-            student.name = Console.ReadLine();
-            student.kurse = int.Parse(Console.ReadLine());
-            student.sex = bool.Parse(Console.ReadLine());
+            student student = new student(Console.ReadLine(), int.Parse(Console.ReadLine()), bool.Parse(Console.ReadLine()));
 
             Console.WriteLine(student.name);
             Console.WriteLine(student.kurse);
